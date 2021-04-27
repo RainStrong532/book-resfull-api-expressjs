316056ed92c421625d2e803f2be1b1ee7a6e295f
Manh vu <vuvanmanhttvtvp2@gmail.com>
2021-04-27 18:23:23 +0700

    set up html

--- index.js
+++ index.js
@@ -3,18 +3,23 @@
 var express = require('express');
 const config = require('./config');
 const cors = require('cors');
-
 var app = express();
 
-const bookRouter = require('./src/router/bookRouter')
-const bookRouterMysql = require('./src/router/bookRouterMysql')
+const bookRouter = require('./src/router/bookRouter');
+const bookRouterMysql = require('./src/router/bookRouterMysql');
+const pageRouter = require('./pageRouter');
+
+
+
+app.use(express.static("/public"));
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/public/books', bookRouter.routes); // http://localhost:8031/api/books url để tạo request
+app.use('/api/books', bookRouter.routes); // http://localhost:8031/api/books url để tạo request
 app.use('/api/mysql/books', bookRouterMysql.routes);
+app.use("/", pageRouter.routes);
 
 app.listen(config.port, () => {
     console.log('Server is running on http://localhost:' + config.port);
---
+++ pageRouter.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const option = {
+    root: path.join(__dirname)
+}
+
+router.get("/", (req, res) => {
+    const fileName = "/public/page/index.html";
+    res.sendFile(fileName, option, (err) => {
+        if(err) res.send(err);
+    })
+})
+
+module.exports = {
+    routes: router //Xuất router
+}
