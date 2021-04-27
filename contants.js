4b6c80ed5c252ca5526fa3f8c65475f7227896b4
Manh vu <vuvanmanhttvtvp2@gmail.com>
2021-04-27 15:56:08 +0700

    back to basic

--- contants.js
+++ contants.js
@@ -13,44 +13,15 @@ const SQL_DATABASE = "book_store";
 
 const FRONTEND_URL = "http://localhost:3000";
 
-//email
-const EMAIL = "your email";
-const MAIL_PASSWORD = "your password";
-
 //mysql config
 const MYSQL_USER = "root";
 const MYSQL_PASSWORD = "mysqlpassword";
 const MYSQL_HOST = "localhost";
 const MYSQL_DATABASE = "book_store";
 
-const VERIFY_EXPRIED_TIME = 600000; // 10 phut
-
-const TOKEN_SECRET = "c6fe37c7d392cf78a76ae4187795519d32833baaab12de9461064fb07f0bcfa17f2305aeabc15ddb6b0c6d86b754c3cc219e7d2749512df01d9f617ca4802b28"; // dùng để bảo mật cho đăng nhập càng phức tạp àng khó hack
-
-const ROLES = {
-    admin: "admin",
-    manager: "manager",
-    monitor: "monitor"
-}
-
-const TYPE_TIME = {
-    MILLISECOND: "MILLISECOND",
-    SECOND: "SECOND",
-    MINUTE: "MINUTE",
-    HOUR: "HOUR",
-    DAY: "DAY",
-    MONTH: "MONTH",
-    YEAR: "YEAR",
-}
-
 module.exports = {
     PORT, HOST, HOST_URL,
     SQL_USER, SQL_SERVER, SQL_PASSWORD, SQL_DATABASE, // SQL Server
     MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_DATABASE, // MySQL
-    TOKEN_SECRET,
-    ROLES,
     FRONTEND_URL,
-    EMAIL, MAIL_PASSWORD, // MAIL
-    VERIFY_EXPRIED_TIME,
-    TYPE_TIME
 }
--- index.js
+++ index.js
@@ -3,50 +3,18 @@
 var express = require('express');
 const config = require('./config');
 const cors = require('cors');
-const path = require('path');
 
 var app = express();
 
 const bookRouter = require('./src/router/bookRouter')
 const bookRouterMysql = require('./src/router/bookRouterMysql')
-const authRouter = require('./src/router/authRouter')
-const bookAuth = require('./src/router/book.auth.router')
-const uploadRouter = require('./src/router/uploadRouter')
-const verifyRouter = require('./src/router/verifyRouter')
 
-const auth = require('./src/middleware/authenticated');
-
-
-app.use(express.static("assets")); // use static file
-app.set('view engine', 'pug');
-app.set('views', './src/view');
-
-app.use(cors({origin: config.front_end_url}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.render('welcome/index');
-})
-app.get('/homepage', (req, res) => {
-    var options = {
-        root: path.join(__dirname)
-    };
-    var fileName = 'assets/pages/index.html';
-    res.sendFile(fileName, options, function (err) {
-        if (err) {
-            res.send("Error")
-        } else {
-        }
-    });
-})
-app.use('/upload', uploadRouter.routes);
-
-app.use('/api/books' ,auth.authenticateToken, bookAuth.routes); // kiểm tra jsonwebtoken
 app.use('/api/public/books', bookRouter.routes); // http://localhost:8031/api/books url để tạo request
 app.use('/api/mysql/books', bookRouterMysql.routes);
-app.use('/api/auth', authRouter.routes);
-app.use('/api/secrets', verifyRouter.routes);
 
 app.listen(config.port, () => {
     console.log('Server is running on http://localhost:' + config.port);
